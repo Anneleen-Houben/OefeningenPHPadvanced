4d9f93fdb2c68d5657efa92891308bcda96d7c2c
Anneleen Houben <anneleen.houben@vdabcampus.be>
2021-07-14 09:54:19 +0200

    oefeningen hoofdstuk 1

---
+++ oef 1/business/UserService.php
@@ -0,0 +1,16 @@
+<?php
+//business/UserService.php
+declare(strict_types=1);
+
+class UserService {
+
+    public function controleerGebruiker(string $gebruikersnaam, string $wachtwoord): bool {
+        if ($gebruikersnaam === "admin" && $wachtwoord === "geheim"){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
---
+++ oef 2/presentation/geheimeinformatie.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<!--presentation/geheimeinformatie.php-->
+<html>
+    <head>
+        <meta charset=utf-8>
+        <title>Geheime informatie</title>
+    </head>
+    <body>
+
+        <h1>Geheime informatie</h1>
+        
+    </body>
+</html>
